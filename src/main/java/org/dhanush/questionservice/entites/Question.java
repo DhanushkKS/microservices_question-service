cac10f8b01b137eb1de923f3df6d9a52c47a950e
Shyaman Dhanushka <ksdhanushka777@gmail.com>
2024-02-20 20:46:12 +0530

    seeded 30 questions to database

--- src/main/java/org/dhanush/questionservice/entites/Question.java
+++ src/main/java/org/dhanush/questionservice/entites/Question.java
@@ -18,11 +18,11 @@ public class Question {
     private int id;
     private String category;
     private String difficultyLevel;
+    private String questionTitle;
     private String option1;
     private String option2;
     private String option3;
     private String option4;
-    private String questionTitle;
     private String rightAnswer;
 
 }
